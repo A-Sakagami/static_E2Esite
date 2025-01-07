b6f3ec8d165ff255b0891ea1f0798e48bd6e4e8d
A-Sakagami <sakagami@genz.jp>
2025-01-07 14:13:46 +0900

    baseURLでスラッシュが重複するのを避けるように調整

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,15 +2,15 @@ window.addEventListener('message', function(event) {
     if (event.origin !== window.location.origin) {
       return;
     }
-
+  
     const authData = event.data;
     const authLink = document.getElementById('authLink');
-
+  
     if (authData.isLoggedIn) {
-      authLink.innerHTML = '<a href="{{ .Site.BaseURL }}login/">ログイン</a>';
+      authLink.innerHTML = '<a href="BASE_URL/logout/">ログアウト</a>';
     } else {
-      authLink.innerHTML = '<a href="{{ .Site.BaseURL }}logout/">ログアウト</a>';
+      authLink.innerHTML = '<a href="BASE_URL/login/">ログイン</a>';
     }
   });
-
+  
   document.getElementById('authFrame').style.display = 'block';
