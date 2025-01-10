2d459d79518b03ecc6af2d153952b50a1d01633b
A-Sakagami <sakagami@genz.jp>
2025-01-10 17:22:54 +0900

    ログイン後の遷移先更新

--- public/js/main.js
+++ public/js/main.js
@@ -11,9 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
           // 簡易的な認証処理
           if (username === "admin" && password === "admin") {
               localStorage.setItem("userType", "admin");
-              alert("ログインしました。");
-              window.location.href = "/";
-          } else {
+              alert("管理者でログインしました。");
+              window.location.href = "{{ .Site.BaseURL }}";
+          } else if (username === "user" && password === "user") {
+              localStorage.setItem("userType", "user");
+              alert("ユーザーでログインしました。");
+              window.location.href = "{{ .Site.BaseURL }}";
+          } else{
               alert("ログインに失敗しました。");
           }
       });
